9bd7d63d89de3e6f898199120fe2db9b8249f83a
Abhinav Damodara <96974003+AbhinavGitam@users.noreply.github.com>
2023-08-14 00:31:55 +0530

    Add files via upload

---
+++ Quiz Website/questions.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "C. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language",
+        ],
+    },
+    {
+        numb: 2,
+        question: "Which programming language is commonly used for adding interactivity and dynamic behavior to websites?",
+        answer: "A. JavaScript",
+        options: [
+            "A. JavaScript",
+            "B. HTML",
+            "C. Python",
+            "D. CSS",
+        ],
+    },
+    {
+        numb: 3,
+        question: "Which of the following is used to style the appearance of web content?",
+        answer: "C. CSS",
+        options: [
+            "A. Hypertext Preprocessor",
+            "B. JavaScript",
+            "C. CSS",
+            "D. HTML",
+        ],
+    },
+    {
+        numb: 4,
+        question: "What does the acronym 'HTTP' stand for in web development?", 
+        answer: "D. Hypertext Transfer Protocol",
+        options: [
+            "A. High Text Transfer Protocol",
+            "B. Hyper Text Markup Language",
+            "C. Hyperlink and Text Transfer Protocol",
+            "D. Hypertext Transfer Protocol",
+        ],
+        
+    },
+    {
+        numb: 5,
+        question: "What does SQL stand for?",
+        answer: "C. Structured Query Language",
+        options: [
+            "A. Strength Query Language",
+            "B. Stylesheet Query Language",
+            "C. Structured Query Language",
+            "D. Science Question Language",
+        ],
+    },
+];
